5a517199ef343fc674e178711ae38d00e3f3980a
Sera Honour <sera.honour@icloud.com>
2022-12-22 20:02:40 +0200

    Moves helper utils to a dedicated helpers folder

---
+++ helpers/settings.py
@@ -0,0 +1,10 @@
+# DO NOT MODIFY THIS FILE UNLESS YOU ARE ADDING NEW VARIABLES!!
+# If you do add a variable here, make sure you update the dictionary in compile.py accordingly
+creator_name = 'CREATOR'
+mods_dir = 'MODS_DIR'
+game_dir = 'GAME_DIR'
+data_dir = 'DATA_DIR'
+python_dir = 'PYTHON_DIR'
+extracted_assets_dir = 'EXTRACTED_ASSETS_DIR'
+interim_mods_dir = 'INTERIM_MODS_DIR'
+hotreload_dir = 'HOTRELOAD_DIR'
