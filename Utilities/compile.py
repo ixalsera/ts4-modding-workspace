4257ea3ef9f8fecd7780b52c0e55fc1b28b6ffd2
Sera Honour <sera.honour@icloud.com>
2022-12-22 17:36:04 +0200

    Allows flagging for injector and settings compilation

--- Utilities/compile.py
+++ Utilities/compile.py
@@ -4,6 +4,9 @@ import settings
 import sys
 from zipfile import PyZipFile, ZIP_STORED
 
+_compile_with_injector = False
+_compile_with_settings = False
+
 
 def compile_module(creator, mod, mods_path):
     """ Compiles the provided mod name to .ts4script
@@ -28,20 +31,43 @@ def compile_module(creator, mod, mods_path):
 
 def show_help():
     """ Displays the help text for this script """
-    print(os.path.basename(__file__) + ' [] mod_name')
+    print(os.path.basename(__file__) + ' [-h|i|s] mod_name')
+    print("""
+Compiling a module is as simple as running compile.py and providing the name of the mod to compile!
+
+To facilitate easier management of mods, since there are two helpers - injector.py and settings.py -
+if you would like to compile your module with support for these modules, you can use the convenience
+options provided.
+
+Options:
+
+-h|--help               Display this help text
+-i|--compile-injector   Include the injector.py helper when compiling the mod
+-s|--compile-settings   Include the settings.py and config.ini when compiling the mod
+""")
 
 
 def main(argv):
+    global _compile_with_injector
+    global _compile_with_settings
     try:
-        opts, args = getopt.getopt(argv, "h")
+        opts, args = getopt.getopt(argv, "his", [
+            'help',
+            'compile-injector',
+            'compile-settings',
+        ])
     except getopt.GetoptError as error:
         print(error)
         show_help()
         sys.exit(2)
     for opt, arg in opts:
-        if opt == '-h':
+        if opt in ('-h', '--help'):
             show_help()
             sys.exit()
+        if opt in ('-i', '--compile-injector'):
+            _compile_with_injector = True
+        if opt in ('-s', '--compile-settings'):
+            _compile_with_settings = True
     if not len(args):
         print('Insufficient arguments provided; at least one mod name must be specified')
         show_help()
