d9528392438d786c5048c751e3c8890e32a6923e
Sera <sera.honour@icloud.com>
2022-12-22 16:04:51 +0200

    Read the config file during validation

--- Utilities/validate_settings.py
+++ Utilities/validate_settings.py
@@ -9,8 +9,8 @@ green = '\033[33;1m'
 reset = '\033[0m'
 
 print(green + 'Current Settings:\n' + reset)
-print(green + 'Creator Name: ' + reset + settings.creator_name)
-print(green + 'Sims 4 Directory: ' + reset + settings.game_dir)
-print(green + 'Sims 4 Mods Directory: ' + reset + settings.mods_dir)
-print(green + 'Extracted assets will be saved in: ' + reset + settings.extracted_assets_dir)
-print(green + 'Work-In-Progress Mods can be located in: ' + reset + settings.interim_mods_dir)
+print(green + 'Creator Name: ' + reset + config['basic']['creator'])
+print(green + 'Sims 4 Directory: ' + reset + config['paths']['game_dir'])
+print(green + 'Sims 4 Mods Directory: ' + reset + config['paths']['mods_dir'])
+print(green + 'Extracted assets will be saved in: ' + reset + config['paths']['extracted_assets_dir'])
+print(green + 'Work-In-Progress Mods can be located in: ' + reset + config['paths']['interim_mods_dir'])
